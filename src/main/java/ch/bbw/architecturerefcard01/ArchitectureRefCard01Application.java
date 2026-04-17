1c2edda76e69d9f47091a3a4709502a7ffad280d
Lorenz Held <held.lorenz.08@gmail.com>
2026-04-17 16:09:54 +0200

    Initiales Commit mit Dockerfile und CI/CD Pipeline

---
+++ src/main/java/ch/bbw/architecturerefcard01/ArchitectureRefCard01Application.java
@@ -0,0 +1,14 @@
+package ch.bbw.architecturerefcard01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureRefCard01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectureRefCard01Application.class, args);
+    }
+
+}
+
